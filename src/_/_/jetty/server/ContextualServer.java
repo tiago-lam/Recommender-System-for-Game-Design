79985a640250ed01c7ac251b8a85fad3c9b5cd4f
Tiago-LA-Machado <tiago.machado@nyu.edu>
2018-04-24 17:28:26 -0400

    sprite set and beginning of sprite inspector implementation

--- src/_/_/jetty/server/ContextualServer.java
+++ src/_/_/jetty/server/ContextualServer.java
@@ -4,12 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
