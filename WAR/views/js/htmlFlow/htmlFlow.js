b90d771d7d5c81e5e5681e372c379c14c3be3fce
Tiago-LA-Machado <tiago.machado@nyu.edu>
2018-10-03 18:46:31 -0400

    set based recommendation

---
+++ WAR/views/js/htmlFlow/htmlFlow.js
@@ -0,0 +1,12 @@
+function goToLoadTab()
+{
+    document.getElementById("levelMap").checked = false;
+    document.getElementById("load").checked = true;
+    document.getElementById("initPageDiv").style.display = "none";
+    document.getElementById("levelDivId").style.display = "block";
+}
+
+function startWithEmptyProject()
+{
+
+}
