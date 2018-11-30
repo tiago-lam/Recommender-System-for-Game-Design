df2baa32ddfa36cf9266d08980dfb0de27bd0a71
Tiago-LA-Machado <tiago.machado@nyu.edu>
2018-11-30 17:41:05 -0500

    methods to manage interaction recommendation

--- src/rec/sys/basics/SaveGamesAsJSON.java
+++ src/rec/sys/basics/SaveGamesAsJSON.java
@@ -4,6 +4,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import _.myParser.ParserGameDescription;
 import rec.sys.catalogue.Catalogue;
 
@@ -20,22 +25,40 @@ public class SaveGamesAsJSON {
 		}
 	}
 	
-	public void printGameJSONString(String gameName) throws IOException
+	public String printGameJSONString(String gameName) throws IOException
 	{
 		ParserGameDescription parser = new ParserGameDescription();
 		String game = parser.getGameObject("examples/gridphysics/" + gameName + ".txt");
-		System.out.println(game);
+		return game;
 	}
 	
-	public static void main(String[] args) throws IOException {
-		SaveGamesAsJSON s = new SaveGamesAsJSON();
+	public JSONArray getGameJSONCollection() throws IOException, ParseException
+	{
+		JSONArray arr = new JSONArray();
+		ParserGameDescription parser = new ParserGameDescription();
 		Catalogue c = new Catalogue();
-		ArrayList<String> arr = c.getGameNames("examples/gridphysics/");
-		for (String g : arr) {
-			g = g.replace(".txt", "");
-			s.printGameJSONString(g);	
+		ArrayList<String> gameNames = c.getGameNames("examples/gridphysics/");
+		for (String game : gameNames) {
+			String gameString = parser.getGameObject("examples/gridphysics/" + game);
+			JSONParser jsonParser = new JSONParser();
+			JSONObject obj = (JSONObject) jsonParser.parse(gameString);
+			arr.add(obj);
 		}
-		
+		return arr;
+	}
+	
+	public void saveAllGames() throws IOException, ParseException
+	{
+		JSONArray arr = getGameJSONCollection();
+		try (FileWriter file = new FileWriter("JSONFiles/games/" + "collection" + ".json")) {
+			file.write(arr.toJSONString());
+			System.out.println("Successfully Copied JSON Object to File...");
+		}
+	}
+	
+	public static void main(String[] args) throws IOException, ParseException {
+		SaveGamesAsJSON s = new SaveGamesAsJSON();
+		s.saveAllGames();
 	}
 
 }
