3e4aeb2b806608e7aaaf1dffb41c27d4f65aceaf
Tiago-LA-Machado <tiago.machado@nyu.edu>
2019-02-23 01:55:51 -0500

    new termination inspector

--- WAR/views/js/termination/showTerminationSetScript.js
+++ WAR/views/js/termination/showTerminationSetScript.js
@@ -1,13 +1,16 @@
 var mapIdToTermination = new Map();
+var mapTerminationIdToTerminationObj = [];
+
 
 function buildTerminationSet(terminationSetObj)
 {
-    var ulElement = document.getElementById('terminationList');
-    for(var i = 0; i < terminationSetObj.length; i++)
-    {
-        var htmlText = convertTerminationObjectToText(terminationSetObj[i]);
-        createDivForThisTerminationTextObj(htmlText, ulElement, 'termination' + i, terminationSetObj[i]);
-    }
+    // var ulElement = document.getElementById('terminationList');
+    // for(var i = 0; i < terminationSetObj.length; i++)
+    // {
+    //     var htmlText = convertTerminationObjectToText(terminationSetObj[i]);
+    //     createDivForThisTerminationTextObj(htmlText, ulElement, 'termination' + i, terminationSetObj[i]);
+    // }
+    renderTerminationList(terminationSetObj);
 }
 
 function convertTerminationObjectToText(obj)
@@ -44,3 +47,108 @@ function getTerminationForUpdatingOnMouseClick(e) {
     var terminationObj = mapIdToTermination.get(e);
     showTerminationInfo(terminationObj, e);
 }
+
+function renderTerminationList(terminationList)
+{
+    for(var i = 0; i < terminationList.length; i++)
+    {
+        var terObj = terminationList[i];
+        convertTerminationObjToHTML(terObj, i);
+    }
+}
+
+function convertTerminationObjToHTML(terminationObj, index) {
+    var parameters = terminationObj["parameters"];
+    var result = ""
+
+    if (parameters['win'] == 'False') {
+        result = "Lose";
+    } else {
+        result = "Win"
+    }
+
+    if (terminationObj["termination"] == "SpriteCounter") {
+
+        var div = document.createElement('div');
+        var divContent =
+            "<div id=termination" + index + " class=terminationContainerSc>\n" +
+            "    <span>Sprite Counter</span>\n" +
+            "    <div>\n" +
+            "        If number of <span>" + parameters['stype'] + "</span> = <span>" + parameters['limit'] + "</span>" +
+            " => <span>" + result + "</span>\n" +
+            "    </div>\n" +
+            "</div>";
+        div.innerHTML = divContent;
+        mapTerminationIdToTerminationObj['termination' + index] = terminationObj;
+        document.getElementById('terminationList').appendChild(div);
+        div.setAttribute("onmousedown", "showTerminationOnInspector('"+ 'termination' + index + "')");
+    }
+
+    if (terminationObj["termination"] == "Timeout") {
+        var div = document.createElement('div');
+        var divContent =
+            "<div id=termination" + index + " class=terminationContainerTo>\n" +
+            "    <span>Time Out</span>\n" +
+            "    <div>\n" +
+            "        If time = <span>" + parameters['limit'] + "</span> => <span>" + result + "</span>\n" +
+            "    </div>\n" +
+            "</div>"
+        div.innerHTML = divContent;
+        mapTerminationIdToTerminationObj['termination' + index] = terminationObj;
+        document.getElementById('terminationList').appendChild(div);
+        div.setAttribute("onmousedown", "showTerminationOnInspector('"+ 'termination' + index + "')");
+    }
+
+    if(terminationObj['termination'] == "MultiSpriteCounter")
+    {
+        var div = document.createElement('div');
+        var divContent =
+            "<div id=termination\" + index + class=terminationContainerMsc>\n" +
+            "    <span>Multi Sprite Counter</span>\n" +
+            "    <div>\n" +
+            "        If number of <span>" + parameters['stype1'] + "</span> or <span>" + parameters['stype2'] + "</span> =" + parameters['limit'] + " <span></span>\n" +
+            "         => <span>" + result + "</span>\n" +
+            "    </div>\n" +
+            "</div>"
+        div.innerHTML = divContent;
+        mapTerminationIdToTerminationObj['termination' + index] = terminationObj;
+        document.getElementById('terminationList').appendChild(div);
+        div.setAttribute("onmousedown", "showTerminationOnInspector('"+ 'termination' + index + "')");
+    }
+
+}
+
+function showTerminationOnInspector(id)
+{
+    var terminationObj = mapTerminationIdToTerminationObj[id];
+    if(terminationObj["termination"] == "SpriteCounter")
+    {
+        renderTerminationCondition('SpriteCounter');
+        document.getElementById('sprite1Id').value = terminationObj['parameters']['stype'];
+        document.getElementById('inputSpriteCounterId').value = terminationObj['parameters']['limit'];
+        var winLose = terminationObj['parameters']['win'] ? "Win" : "Lose";
+        document.getElementById('gameOverScId').value = winLose;
+
+    }
+    if(terminationObj["termination"] == "Timeout")
+    {
+        renderTerminationCondition("TimeOut");
+        document.getElementById('timeOutInputId').value = terminationObj['parameters']['limit'];
+        var winLose =  terminationObj['parameters']['win'] ? "Win" : "Lose";
+        document.getElementById('gameOverToId').value = winLose;
+    }
+
+    if(terminationObj["termination"] == "MultiSpriteCounter")
+    {
+        renderTerminationCondition("MultiSpriteCounter");
+        document.getElementById('sprite1mscId').value = terminationObj['parameters']['stype1'];
+        document.getElementById('sprite2mscId').value = terminationObj['parameters']['stype2'];
+        document.getElementById('inputMspriteCounterId').value = terminationObj['parameters']['limit'];
+        var winLose = terminationObj['parameters']['win'] ? "Win" : "Lose";
+        document.getElementById('gameOverMscId').value = winLose;
+    }
+
+}
+
+
+
